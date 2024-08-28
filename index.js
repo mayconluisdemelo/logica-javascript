e238418ccd104711eb0551224a00ba0cb4d2e225
mayconluisdemelo <165900252+mayconluisdemelo@users.noreply.github.com>
2024-08-28 07:59:01 -0300

    javascript

---
+++ index.js
@@ -0,0 +1,37 @@
+var nome = "john";
+
+let idade = 25;
+
+const pi = 3.14;
+
+let a = 5;
+let b = 10;
+let soma = a + b; 
+console.log(soma); 
+
+let a = 5;
+let b = 10;
+
+function multiplica (x, y){
+    return x * y;
+}
+
+let prouduto = multiplica (a, b); 
+console.log(produto); 
+
+let frutas = ['Apple', 'Banana', 'cherry'];
+console.log (frutas[1]); 
+
+let pessoa = {
+
+primeiroNome: 'John',
+sobrenome: 'Delon',
+age: 15
+};
+
+console.log(pessoa.primeiroNome); 
+ 
+
+
+
+
